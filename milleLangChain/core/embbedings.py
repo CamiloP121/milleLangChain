38fcd2e0df8f7c87ccde77ccbfb034937c9be917
CamiloP121 <andresospino14@gmail.com>
2024-08-15 13:58:56 -0500

    fix words

--- milleLangChain/core/embbedings.py
+++ milleLangChain/core/embbedings.py
@@ -1,6 +1,6 @@
-from langchain_community.embeddings.spacy_embeddings import Spacyembeddings
-from langchain_openai import OpenAIembeddings
-from langchain_community.embeddings import GPT4Allembeddings
+from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import GPT4AllEmbeddings
 
 from milleLangChain.utils import print_helpers as pp
 
@@ -25,12 +25,12 @@ class embedding():
         allowed_types = ["spacy", "openia", "gpt4"]
 
         if "spacy" in embedding_type:
-            self.model = Spacyembeddings(model_name=model)
+            self.model = SpacyEmbeddings(model_name=model)
         elif "openia" in embedding_type:
             assert key is not None and key != "", "OpenAI API key is required for 'openia' embedding type."
-            self.model = OpenAIembeddings(openai_api_key=key, model=model)
+            self.model = OpenAIEmbeddings(openai_api_key=key, model=model)
         elif "gpt4" in embedding_type:
-            self.model = GPT4Allembeddings(model_name=model)
+            self.model = GPT4AllEmbeddings(model_name=model)
         
         else:
             raise Exception("embedding type must be one of: " + " or ".join(allowed_types))
